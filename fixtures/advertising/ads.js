1c32525824f0038dc1439a9422ef75e0f3f9ec5a
Siratee K <sirateek@gmail.com>
2023-02-25 14:30:26 +0700

    fix: Add fixture for advertising service

---
+++ fixtures/advertising/ads.js
@@ -0,0 +1,24 @@
+const mongodb = require("mongodb");
+
+module.exports = [
+  {
+    name: "Agoda",
+    link: "https://www.agoda.com/",
+  },
+  {
+    name: "Google",
+    link: "https://google.com",
+  },
+  {
+    name: "LINE",
+    link: "https://line.me",
+  },
+  {
+    name: "Siratee K",
+    link: "https://sirateek.dev",
+  },
+  {
+    name: "Facebook",
+    link: "https://facebook.com",
+  },
+];
